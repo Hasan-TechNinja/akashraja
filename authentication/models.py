d304bd16cce2b61beae09a9bbb6780d84282fed9
Hasan-TechNinja <hasantechninja@gmail.com>
2025-10-18 10:02:46 +0600

    profile generate the registrations time

--- authentication/models.py
+++ authentication/models.py
@@ -13,17 +13,8 @@ class EmailVerification(models.Model):
         return timezone.now() > self.created_at + timedelta(minutes=3)
 
 
-# class PasswordResetCode(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     code = models.CharField(max_length=6)
-#     created_at = models.DateTimeField(auto_now_add=True)
-
-#     def is_expired(self):
-#         return timezone.now() > self.created_at + timedelta(minutes=3)
-
-
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
     name = models.TextField(max_length=200, default='Your Name')
     phone = models.CharField(max_length=50, blank=True, null=True)
     image = models.ImageField(upload_to="profile", blank=True, null=True)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='categories/')
+    
+    def __str__(self):
+        return self.name
+    
+class Option(models.Model):
+    category = models.ForeignKey(Category, related_name='options', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='options/')
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
