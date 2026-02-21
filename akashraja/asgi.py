cd552ff0614f9ea927e057c87ad3e96d041225cc
Hasan-TechNinja <hasantechninja@gmail.com>
2026-02-21 14:13:49 +0600

    bug

--- akashraja/asgi.py
+++ akashraja/asgi.py
@@ -1,13 +1,17 @@
 # asgi.py
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'akashraja.settings')
+
+# Standard Django ASGI app for HTTP
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.sessions import SessionMiddlewareStack
 
 from game.middleware import JWTAuthMiddleware           # <-- your custom JWT middleware
 from game.routing import websocket_urlpatterns           # <-- your WS URLs
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'akashraja.settings')
 
 # Standard Django ASGI app for HTTP
 django_asgi_app = get_asgi_application()
--- game/middleware.py
+++ game/middleware.py
@@ -1,12 +1,9 @@
 # game/middleware.py
 import jwt
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from channels.db import database_sync_to_async
 from urllib.parse import parse_qs
 from django.utils.deprecation import MiddlewareMixin
-
-User = get_user_model()
 
 class MyMiddleware(MiddlewareMixin):
     def process_request(self, request):
@@ -55,6 +52,8 @@ class JWTAuthMiddleware:
 
     @database_sync_to_async
     def get_user(self, user_id):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
         try:
             return User.objects.get(id=user_id)
         except User.DoesNotExist:
